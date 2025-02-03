26c077bb9a9f70a6ea23cbf3a90d75dfffedf94d
Rohan Godha <git@rohangodha.com>
2025-02-03 14:28:43 -0500

    add tailwind

--- moviessite/urls.py
+++ moviessite/urls.py
@@ -18,4 +18,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("/", include("movies.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("movies.urls"))]
