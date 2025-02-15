a92a3dc88e8ff62683df7ff7f512bbc5f8b298d3
Rohan Godha <git@rohangodha.com>
2025-02-15 17:34:00 -0500

    switch smtp to gmail

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -141,9 +141,8 @@ MEDIA_URL = "/media/"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.resend.com"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "resend"
-EMAIL_HOST_PASSWORD = env("RESEND_API_KEY")
+EMAIL_HOST_USER = "2340movies1234@gmail.com"
+EMAIL_HOST_PASSWORD = env("GMAIL_PASSWORD")
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "noreply@2340movies.rgodha.com"
